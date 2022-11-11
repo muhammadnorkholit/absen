b3f3ba840e2960fcdcaef89464cfef3e951b2939
BabuTugas <gamingindonesia0768@gmail.com>
2022-11-11 01:19:48 -0800

    print

--- app/Http/Controllers/PrintpdfController.php
+++ app/Http/Controllers/PrintpdfController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Dompdf\Dompdf;
 use Illuminate\Http\Request;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
@@ -14,6 +15,19 @@ class PrintpdfController extends Controller
 
     public function print()
     {
-        return view('dashboard.printpdf');
+        $html =  view('dashboard.printpdf');
+
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render('dashboard.printpdf');
+
+        // Output the generated PDF to Browser
+        $dompdf->stream('dashboard.printpdf');
+        // return view('dashboard.printpdf');
     }
 }
