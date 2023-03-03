051e0118bda9656dcf7e9d0adf62a2269595e1e4
muhammadnorkholit <mnorkholit7@gmail.com>
2023-03-03 15:20:16 +0700

    p

--- app/Imports/SiswaImport.php
+++ app/Imports/SiswaImport.php
@@ -25,6 +25,18 @@ class SiswaImport implements ToModel, WithHeadingRow
             $id = 0;
         }
 
+        $month = date('m');
+        if($month <= '06'){
+            $tahun = date('Y',strtotime("-1 Year"))."/".date('Y');;
+            $semester = "ganjil";
+        }else{
+             $tahun = date('Y')."/".date('Y',strtotime("+1 year"));
+            $semester = "genap";
+        }
+
+        $id_Ajaran = DB::table('tahun_ajaran')->where('tahun',$tahun)->where('semester',$semester)->first()->id;
+        if(!$id_Ajaran)return;
+
         DB::table('siswa')->insert([
             'nama' =>  $row['nama'],
             'nisn' => $row['nisn'],
@@ -32,7 +44,8 @@ class SiswaImport implements ToModel, WithHeadingRow
             'no_kelas' => $row['no_kelas'],
             'id_ruangan' => $row['ruangan'],
             'sesi' => $row['sesi'],
-            'id_jurusan' => $id
+            'id_jurusan' => $id,
+            'id_ajaran'=>$id_Ajaran
         ]);
     }
 }
